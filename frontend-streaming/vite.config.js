8b975f4d1c439631bbd16fa601e7449b6e59ef4a
Leonardo <leonardo@conectra.com.br>
2026-06-20 17:13:18 -0300

    feat: adiciona CORS no Program.cs e atualiza configuração do Vite

--- frontend-streaming/vite.config.js
+++ frontend-streaming/vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 5173,
+    strictPort: false, // se 5173 ocupada, usa 5174 (já liberada no backend)
+  },
 })
